c5684c02c2f679651f11f781207fb00cad1ae6b7
Ronald Peña <126174369+gerogpena@users.noreply.github.com>
2023-06-01 02:03:32 +0800

    Update settings.py

--- itms/settings.py
+++ itms/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'it_prof',
     'ticket_system',
     'pages',
     'users_account',
